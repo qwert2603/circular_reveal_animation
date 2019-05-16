eda4069b23fd12dd418de121e69848af792bb4a1
qwert2603 <qwert2603@mail.ru>
2019-05-16 11:34:28 +0700

    Initial commit

---
+++ example/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:circular_reveal_animation/circular_reveal_animation.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'CRA Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  AnimationController animationController;
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
+    animation = CurvedAnimation(
+      parent: animationController,
+      curve: Curves.easeIn,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("CRA Demo"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: CircularRevealAnimation(
+          minRadius: 12,
+          maxRadius: 200,
+          center: Offset(0, 300),
+          child: Container(color: Colors.red),
+          animation: animation,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        if (animationController.status == AnimationStatus.forward ||
+            animationController.status == AnimationStatus.completed) {
+          animationController.reverse();
+        } else {
+          animationController.forward();
+        }
+      }),
+    );
+  }
+}
---
+++ lib/circular_reveal_animation.dart
@@ -0,0 +1,39 @@
+library circular_reveal_animation;
+
+import 'package:flutter/material.dart';
+
+import 'circular_reveal_clipper.dart';
+
+class CircularRevealAnimation extends StatelessWidget {
+  final Offset center;
+  final double minRadius;
+  final double maxRadius;
+  final Widget child;
+  final Animation<double> animation;
+
+  CircularRevealAnimation({
+    this.center,
+    this.minRadius,
+    this.maxRadius,
+    @required this.child,
+    @required this.animation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: animation,
+      builder: (BuildContext context, Widget _) {
+        return ClipPath(
+          clipper: CircularRevealClipper(
+            fraction: animation.value,
+            center: center,
+            minRadius: minRadius,
+            maxRadius: maxRadius,
+          ),
+          child: this.child,
+        );
+      },
+    );
+  }
+}
